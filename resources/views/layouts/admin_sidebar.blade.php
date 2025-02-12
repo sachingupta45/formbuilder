5c373f67cf72358a4cbb68e5c80e275b1d45bd25
mohankr7870 <149649826+mohankr7870@users.noreply.github.com>
2025-02-13 04:16:17 +0530

    show the action permission wise

--- resources/views/layouts/admin_sidebar.blade.php
+++ resources/views/layouts/admin_sidebar.blade.php
@@ -10,9 +10,11 @@
                 <li class="dropdown">
                   <a href="{{route('admin.dashboard')}}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
                 </li>
-                <li @class(['dropdown','active'=>request()->routeIs('admin.form.*')])>
-                  <a href="{{route('admin.form.index')}}" class="nav-link"><i data-feather="code"></i><span>Form</span></a>
-                </li>
+                @can('form-view')
+                    <li @class(['dropdown','active'=>request()->routeIs('admin.form.*')])>
+                    <a href="{{route('admin.form.index')}}" class="nav-link"><i data-feather="code"></i><span>Form</span></a>
+                    </li>
+                @endcan
                 <li @class(['dropdown','active'=>request()->routeIs('admin.data.*')]) >
                   <a href="{{route('admin.role.index')}}" class="nav-link"><i data-feather="codepen"></i><span>data</span></a>
                 </li>
